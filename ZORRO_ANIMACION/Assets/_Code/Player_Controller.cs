2a6714c459761691ceb4ba30775740b96cede7f9
Fernando747 <fernandojcr5@gmail.com>
2022-07-07 23:24:53 -0500

    Codigo caminata

---
+++ ZORRO_ANIMACION/Assets/_Code/Player_Controller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Animator _playerAnimator;
+
+    [Header("Settings")]
+    [SerializeField] private float _Speed;
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            _characterController.Move(Vector3.right * _Speed * Time.deltaTime);
+            _playerAnimator.SetBool("Walking_right", true);
+            _playerAnimator.SetBool("Idle", false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _characterController.Move(Vector3.left * _Speed * Time.deltaTime);
+        }
+
+        _playerAnimator.SetBool("Idle",true);
+        _playerAnimator.SetBool("Walking_right", false);
+    }
+}
