9df5f0deed4f93449d9762a2e19f33944ad7aba0
Mauricio Méndez <mauvandesigner@gmail.com>
2022-07-15 21:37:17 -0500

    Cambios en codigo

--- ZORRO_ANIMACION/Assets/_Code/Player_Controller.cs
+++ ZORRO_ANIMACION/Assets/_Code/Player_Controller.cs
@@ -10,7 +10,7 @@ public class Player_Controller : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float _Speed;
-
+    private bool _IsWalkingRight;
     private void Update()
     {
         CheckInput();
@@ -20,17 +20,38 @@ public class Player_Controller : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
+            _IsWalkingRight = true;
             _characterController.Move(Vector3.right * _Speed * Time.deltaTime);
             _playerAnimator.SetBool("Walking_right", true);
-            _playerAnimator.SetBool("Idle", false);
+            _playerAnimator.SetBool("Idle_rigth", false);
         }
-
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
+            _IsWalkingRight = false;
             _characterController.Move(Vector3.left * _Speed * Time.deltaTime);
+            _playerAnimator.SetBool("Idle_rigth", false);
+            _playerAnimator.SetBool("Walking_left", true);
+        }
+        else
+        {
+            if(_IsWalkingRight ==true)
+            {
+                _playerAnimator.SetBool("Idle_rigth", true);
+                _playerAnimator.SetBool("Idle_left", false);
+                _playerAnimator.SetBool("Walking_left", false);
+                _playerAnimator.SetBool("Walking_right", false);
+            }else if(_IsWalkingRight == false)
+            {
+                _playerAnimator.SetBool("Idle_left", true);
+                _playerAnimator.SetBool("Idle_rigth", false);
+                _playerAnimator.SetBool("Walking_left", false);
+                _playerAnimator.SetBool("Walking_right", false);
+            }
         }
 
-        _playerAnimator.SetBool("Idle",true);
-        _playerAnimator.SetBool("Walking_right", false);
+
+
+
+
     }
 }
