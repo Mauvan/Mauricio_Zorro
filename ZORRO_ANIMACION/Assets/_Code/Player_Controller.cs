6b2d0624c7a47a27820258f4c40b59bd20e548e7
Fernando747 <fernandojcr5@gmail.com>
2022-07-15 22:10:47 -0500

    Code changes

--- ZORRO_ANIMACION/Assets/_Code/Player_Controller.cs
+++ ZORRO_ANIMACION/Assets/_Code/Player_Controller.cs
@@ -6,11 +6,21 @@ public class Player_Controller : MonoBehaviour
 {
     [Header("Dependencies")]
     [SerializeField] private CharacterController _characterController;
-    [SerializeField] private Animator _playerAnimator;
+    [SerializeField] private GameObject _leftCharacter;
+    [SerializeField] private Animator _leftAnimator;
+    [SerializeField] private GameObject _rightCharacter;
+    [SerializeField] private Animator _rightAnimator;
 
     [Header("Settings")]
     [SerializeField] private float _Speed;
-    private bool _IsWalkingRight;
+
+    private bool _IsWalkingRight = true;
+    private void Start()
+    {
+        _rightCharacter.SetActive(true);
+        _leftCharacter.SetActive(false);
+    }
+
     private void Update()
     {
         CheckInput();
@@ -21,37 +31,35 @@ public class Player_Controller : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             _IsWalkingRight = true;
+            _rightCharacter.SetActive(true);
+            _leftCharacter.SetActive(false);
             _characterController.Move(Vector3.right * _Speed * Time.deltaTime);
-            _playerAnimator.SetBool("Walking_right", true);
-            _playerAnimator.SetBool("Idle_rigth", false);
+            _rightAnimator.SetBool("Walking_right", true);
+            _rightAnimator.SetBool("Idle_rigth", false);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             _IsWalkingRight = false;
+            _leftCharacter.SetActive(true);
+            _rightCharacter.SetActive(false);
             _characterController.Move(Vector3.left * _Speed * Time.deltaTime);
-            _playerAnimator.SetBool("Idle_rigth", false);
-            _playerAnimator.SetBool("Walking_left", true);
-        }
-        else
+            _leftAnimator.SetBool("Walking_left", true);
+            _leftAnimator.SetBool("Idle_left", false);
+        } else
         {
             if(_IsWalkingRight ==true)
             {
-                _playerAnimator.SetBool("Idle_rigth", true);
-                _playerAnimator.SetBool("Idle_left", false);
-                _playerAnimator.SetBool("Walking_left", false);
-                _playerAnimator.SetBool("Walking_right", false);
+                _rightAnimator.SetBool("Idle_rigth", true);
+                _leftAnimator.SetBool("Idle_left", false);
+                _leftAnimator.SetBool("Walking_left", false);
+                _rightAnimator.SetBool("Walking_right", false);
             }else if(_IsWalkingRight == false)
             {
-                _playerAnimator.SetBool("Idle_left", true);
-                _playerAnimator.SetBool("Idle_rigth", false);
-                _playerAnimator.SetBool("Walking_left", false);
-                _playerAnimator.SetBool("Walking_right", false);
+                _leftAnimator.SetBool("Idle_left", true);
+                _rightAnimator.SetBool("Idle_rigth", false);
+                _leftAnimator.SetBool("Walking_left", false);
+                _rightAnimator.SetBool("Walking_right", false);
             }
         }
-
-
-
-
-
     }
 }
