702d0ebe910cf5ca402bf5055350f1aef2b96ff1
Fernando747 <fernandojcr5@gmail.com>
2022-07-29 23:36:27 -0500

    Caminata funcional

--- ZORRO_ANIMACION/Assets/_Code/Player_Controller.cs
+++ ZORRO_ANIMACION/Assets/_Code/Player_Controller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     [Header("Dependencies")]
-    [SerializeField] private CharacterController _characterController;
+    //[SerializeField] private CharacterController _characterController;
     [SerializeField] private GameObject _leftCharacter;
     [SerializeField] private Animator _leftAnimator;
     [SerializeField] private GameObject _rightCharacter;
@@ -14,17 +14,59 @@ public class Player_Controller : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private float _Speed;
 
+    private Rigidbody2D _playerRigid;
     private bool _IsWalkingRight = true;
     private void Start()
     {
+        _playerRigid = this.GetComponent<Rigidbody2D>();
         _rightCharacter.SetActive(true);
         _leftCharacter.SetActive(false);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        CheckInput();
+        GetInput();
+        //CheckInput();
     }
+
+    private void GetInput()
+    {
+      if (Input.GetKey(KeyCode.D))
+        {
+            _playerRigid.velocity = new Vector2(100f * _Speed * Time.deltaTime, 0);
+            _IsWalkingRight = true;
+            _rightCharacter.SetActive(true);
+            _rightAnimator.SetBool("Walking_right", true);
+            _rightAnimator.SetBool("Idle_right", false);
+            _leftCharacter.SetActive(false);
+            
+        } else if (Input.GetKey(KeyCode.A)){
+            _playerRigid.velocity = new Vector2(100f * -_Speed * Time.deltaTime, 0);
+            _IsWalkingRight = false;
+            _leftCharacter.SetActive(true);
+            _leftAnimator.SetBool("Walking_left", true);
+            _leftAnimator.SetBool("Idle_left", false);
+            _rightCharacter.SetActive(false);
+            
+        } else if (!Input.anyKey)
+        {
+            if(_IsWalkingRight)
+            {
+                _rightCharacter.SetActive(true);
+                _rightAnimator.SetBool("Walking_right", false);
+                _rightAnimator.SetBool("Idle_right", true);
+                _leftCharacter.SetActive(false);
+            }
+            else if (!_IsWalkingRight)
+            {
+                _leftCharacter.SetActive(true);
+                _leftAnimator.SetBool("Walking_left", false);
+                _leftAnimator.SetBool("Idle_left", true);
+                _rightCharacter.SetActive(false);
+            }
+        }
+    }
+
 
     private void CheckInput()
     {
@@ -32,7 +74,7 @@ public class Player_Controller : MonoBehaviour
         {
             _IsWalkingRight = true;
             _rightCharacter.SetActive(true);
-            _characterController.Move(Vector3.right * _Speed * Time.deltaTime);
+            //_characterController.Move(Vector3.right * _Speed * Time.deltaTime);
             _rightAnimator.SetBool("Walking_right", true);
             _rightAnimator.SetBool("Idle_rigth", false);
             _leftAnimator.SetBool("Walking_left", false);
@@ -43,7 +85,7 @@ public class Player_Controller : MonoBehaviour
         {
             _IsWalkingRight = false;
             _leftCharacter.SetActive(true);
-            _characterController.Move(Vector3.left * _Speed * Time.deltaTime);
+           // _characterController.Move(Vector3.left * _Speed * Time.deltaTime);
             _leftAnimator.SetBool("Walking_left", true);
             _leftAnimator.SetBool("Idle_left", false);
             _rightAnimator.SetBool("Walking_right", false);
